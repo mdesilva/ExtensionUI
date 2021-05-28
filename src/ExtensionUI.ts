619326ad01ca05381c132eb3ed0afa4520225489
Manuja DeSilva <mdesilva@bu.edu>
2021-05-28 15:23:13 -0400

    added explanatory comment for inner child loop

--- src/ExtensionUI.ts
+++ src/ExtensionUI.ts
@@ -16,6 +16,13 @@ export default class ExtensionUI {
             })
         }
         children.map(child => {
+            /*
+            When child element(s) are passed in as props to functional component, 
+            in the next recursive frame it becomes an array of those child(s),
+            which is then added onto the children parameter, which itself is an array of an indeterminate number of arguments. 
+            So when we hit a child in the children parameter that is not an element or a string, but an
+            array itself, we need to loop through that inner array and append its nodes.
+            */
             if (Array.isArray(child)) {
                 child.map(subchild => {
                     element.append(subchild);
